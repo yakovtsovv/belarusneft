8b937c0123f6089ac10de8be108fdf57a114a859
yakovtsovv <57229878+yakovtsovv@users.noreply.github.com>
2022-01-29 20:58:18 +0300

    update and fix

--- index.php
+++ index.php
@@ -9,6 +9,29 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="profile-user">
+    <div class="window-information">
+        <div class="info">
+            <h3 class="information-title">Общая информация</h3>
+            <div class="info-block">
+                <div class="statistics-user">
+                    <span class="text-label">Имя:</span>
+                </div>
+                <div class="statistics-user">
+                    <span class="text-label">Фамилия:</span>
+                </div>
+                <div class="statistics-user">
+                    <span class="user-money">Баланс: 0 BYN</span>
+                </div>
+            </div>
+            <div class="photo-user"></div>
+        </div>
+        <h3 class="information-title">Последний чек</h3>
+        <div class="info-transaction">
+            <textarea disabled class="window-transaction"></textarea>
+        </div>
+    </div>
+</div>
 <div class="header">
     <div class="logo">
         <img id="img-logo" src="https://www.belorusneft.by/sitebeloil/system/modules/by.iba.beloil.templates/resources/i/b-logo.png">
@@ -23,20 +46,17 @@
             <option>Луговая ул. 2д, Гомель</option>
         </select>
     </div>
-    <div class="money">
-        <h1 class="user-money">0<span> BYN</span></h1>
-    </div>
 </div>
 <div class="menu">
     <div class="field-benzine">
         <select class="number-station">
-            <option hidden disabled selected>Выберите номер колонки</option>
+            <option hidden disabled selected value="select-number-station">Выберите номер колонки</option>
             <option value="station-one">Колонка №1</option>
             <option value="station-two">Колонка №2</option>
             <option value="station-three">Колонка №3</option>
             <option value="station-four">Колонка №4</option>
         </select>
-        <input id="field-liters-benzine" type="number" min="1" max="100" placeholder="Введите количество литров бензина">
+        <input class="field-liters-benzine" type="number" min="1" max="100" placeholder="Введите количество литров бензина">
         <br>
         <div class="option">
             <input class="gas-button" data="2.00" type="button" value="AИ-92">
@@ -62,6 +82,69 @@
                 <td>2.10 BYN</td>
                 <td>2.24 BYN</td>
                 <td>2.10 BYN</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
         </table>
     </div>
--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,45 @@ let textMoneyUser = document.querySelector(".user-money");
 let numberStation = document.querySelector(".number-station");
 let gasOption = document.querySelectorAll(".gas-button");
 
-let userMoney = 50;
+let userMoney = 100;
 
-textMoneyUser.innerHTML = userMoney + " BYN";
+textMoneyUser.innerHTML = "Ваш баланс: " + userMoney + " BYN";
 
 for (let i = 0; i < gasOption.length; i++){
     gasOption[i].onclick = function (){
-        let amountBenzine = document.getElementById("field-liters-benzine").value;
+        let amountBenzine = document.querySelector(".field-liters-benzine").value;
         let priceBenzine = this.getAttribute("data");
         let calculation = amountBenzine * priceBenzine;
         if(amountBenzine == "" || numberStation.selectedIndex == 0){
-            alert("Ошибка! Вы не выбрали номер колонки или не указали кол-во бензина.");
+            alert("Ошибка! Вы не выбрали номер колонки или не указали количество бензина.");
             return false;
         }
         let dialog = confirm("Вы выбрали колонку №" + numberStation.selectedIndex + ". " + "Стоимость бензина " + gasOption[i].value + " за " + amountBenzine + " литр(а/ов)" + " = " + calculation.toFixed(2) + " BYN");
         if(dialog){
-            if(userMoney >= amountBenzine * priceBenzine){
-                userMoney-=amountBenzine * priceBenzine;
-                textMoneyUser.innerHTML = userMoney.toFixed(2) + " BYN";
+            if(userMoney >= calculation){
+                userMoney-=calculation;
+                textMoneyUser.innerHTML = "Ваш баланс: " + userMoney.toFixed(2) + " BYN";
                 alert("Вы приобрели " + gasOption[i].value + " бензин. Литров: " + amountBenzine + ". Стоимость: " + calculation.toFixed(2) + " BYN");
                 console.log("Осуществляется заправка " + gasOption[i].value + " бензина. Литров: " + amountBenzine + " за " + calculation.toFixed(2) + " BYN");
-                document.getElementById("field-liters-benzine").value = "";
+                document.querySelector(".field-liters-benzine").value = "";
+                let date = new Date();
+                let windowTransaction = document.querySelector(".window-transaction").value = "============КАССОВЫЙ ЧЕК============" + "\n\n" + "Колонка №" + numberStation.selectedIndex + "\n" + "Дата и время: " + date.getDate() + "." + date.getMonth()+1 + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + "\n" + "Тип бензина: " + gasOption[i].value + "\n" + "Цена: " + calculation.toFixed(2) + " BYN" + "\n\n" + "====Счастливого пути! Ждем Вас снова!====";
+                document.querySelector(".number-station").value = numberStation.options[0].value;
             }else{
                 alert("У Вас не достаточно средств.");
-                document.getElementById("field-liters-benzine").value = "";
+                document.querySelector(".field-liters-benzine").value = "";
+                document.querySelector(".number-station").value = numberStation.options[0].value;
             }
         }else{
-            alert("Мы ждем Вас снова на заправке Беларуснефть!");
-            document.getElementById("field-liters-benzine").value = "";
+            alert("Мы будем снова рады видеть Вас на АЗС Беларуснефть!");
+            document.querySelector(".field-liters-benzine").value = "";
+            document.querySelector(".number-station").value = numberStation.options[0].value;
         }
     }
+}
+
+function addMoney(amountMoney){
+    userMoney+=parseInt(amountMoney);
+
+    return textMoneyUser.innerHTML = "Ваш баланс: " + userMoney + " BYN";
 }
